5ea54af8f7d0cdb1ad8155676c164bf2a9eea1b1
Tompsaman <valtteri.acer@gmail.com>
2023-06-07 11:17:25 +0300

    Edistymistä

--- NUO-TAISTELU/NUO-TAISTELU/NUO-TAISTELU.cs
+++ NUO-TAISTELU/NUO-TAISTELU/NUO-TAISTELU.cs
@@ -9,21 +9,23 @@ namespace NUO_TAISTELU;
 
 public class NUO_TAISTELU : PhysicsGame
 {
-    Image tausta = LoadImage("tausta");
+    Image tausta_uusi = LoadImage("tausta_uusi");
     Image olionKuva = LoadImage("olionKuva");
     Image viisi = LoadImage("viisi");
     Image failcard = LoadImage("failcard");
     Image kuusi = LoadImage("kuusi");
     Image kahdeksan = LoadImage("kahdeksan");
+    Image ukko = LoadImage("ukko");
+    private Image NUO = LoadImage("NUO");
 
     private void LuoKentta()
     {
-        Level.Background.Image = tausta;
+        Level.Background.Image = tausta_uusi;
         Level.Background.ScaleToLevelFull();
         IsFullScreen = true;
 
         Camera.ZoomToLevel();
-        double paikka = 140;
+        double paikka = - 100;
         LuoKortti(kuusi,paikka);
         double paikkamuutos = 170;
         paikka = paikka + paikkamuutos;
@@ -49,14 +51,34 @@ public class NUO_TAISTELU : PhysicsGame
     {
         LuoKentta();
         
-        PhysicsObject kasi = new PhysicsObject(270, 480);
+        PhysicsObject kasi = new PhysicsObject(200, 400);
         
-        kasi.X = kasi.X + 830;
-        kasi.Y = kasi.Y - 350;
+        kasi.X = kasi.X + 600;
+        kasi.Y = kasi.Y - 300;
         
         kasi.Image = olionKuva;
 
         Add(kasi);
+        
+        PhysicsObject äijä = new PhysicsObject(400, 413);
+        
+        äijä.X = äijä.X + - 400;
+        äijä.Y = äijä.Y + 200;
+        
+        äijä.Image = ukko;
+
+        Add(äijä);
+        äijä.Angle = Angle.FromDegrees(14);
+        
+        PhysicsObject pakka = new PhysicsObject(100, 160);
+        
+        pakka.X = pakka.X + - 0;
+        pakka.Y = pakka.Y + 0;
+        
+        pakka.Image = NUO;
+
+        Add(pakka);
+        pakka.Angle = Angle.FromDegrees(14);
 
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
