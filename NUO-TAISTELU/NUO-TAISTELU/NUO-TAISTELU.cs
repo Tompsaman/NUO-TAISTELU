2443ef216744f558ffb23091ae69cb58d5ea07a7
Tompsaman <valtteri.acer@gmail.com>
2023-06-08 11:30:03 +0300

    joo

--- NUO-TAISTELU/NUO-TAISTELU/NUO-TAISTELU.cs
+++ NUO-TAISTELU/NUO-TAISTELU/NUO-TAISTELU.cs
@@ -9,6 +9,7 @@ namespace NUO_TAISTELU;
 
 public class NUO_TAISTELU : PhysicsGame
 {
+    private Double EVENT_INTERVAL = 1;
     Image tausta_uusi = LoadImage("tausta_uusi");
     Image olionKuva = LoadImage("olionKuva");
     Image viisi = LoadImage("viisi");
@@ -19,29 +20,30 @@ public class NUO_TAISTELU : PhysicsGame
     Image NUO = LoadImage("NUO");
     Image nolla = LoadImage("nolla");
     Image phone = LoadImage("phone");
-    
+    Image puhe1 = LoadImage("puhe1");
+    Image pistooli = LoadImage("pistooli");
+
     DoubleMeter alaspainlaskuri;
     Timer aikalaskuri;
     private int lasku2 = 1;
 
     private PhysicsObject kasi;
+    private PhysicsObject äijä;
     
     void LuoAikalaskuri()
     {
-        alaspainlaskuri = new DoubleMeter(20);
-    
+        alaspainlaskuri = new DoubleMeter(EVENT_INTERVAL);
+        
         aikalaskuri = new Timer();
-        aikalaskuri.Interval = 2;
+        aikalaskuri.Interval = EVENT_INTERVAL;
         aikalaskuri.Timeout += LaskeAlaspain;
+        aikalaskuri.SecondCounterStep = -1;
         aikalaskuri.Start();
-        
-        
-        
         Label aikanaytto = new Label();
         aikanaytto.TextColor = Color.White;
         aikanaytto.DecimalPlaces = 1;
-        aikanaytto.BindTo(alaspainlaskuri);
-        aikanaytto.X = 670;
+        aikanaytto.BindTo(aikalaskuri.SecondCounter);
+        aikanaytto.X = 970;
         aikanaytto.Y = 370;
         Add(aikanaytto, 1);
         
@@ -49,26 +51,40 @@ public class NUO_TAISTELU : PhysicsGame
     }
     void LaskeAlaspain()
     {
+        aikalaskuri.SecondCounter.Reset();
         switch (lasku2)
         {
             case 1:
                 kasi.Image = phone;
                 break;
-            case 2:
-                ukko.Image = phone;
+            case 2: 
+                äijä.Image = puhe1;
+                break;
+            case 3:
+                PhysicsObject ase = new PhysicsObject(100, 160);
+        
+                ase.X = ase.X - 120;
+                ase.Y = ase.Y + 200;
+        
+                ase.Image = pistooli;
+
+                Add(ase);
+                ase.Angle = Angle.FromDegrees(14);
+                break;
+            case 4:
+                var rajahdys = new Explosion(900);
+                rajahdys.Position = new Vector(+ 100, - 20);
+                rajahdys.UseShockWave = true;
+                Add(rajahdys);
                 break;
         }
         kasi.Image = phone;
+        ukko = puhe1;
         lasku2++;
     }
 
     private void LuoKentta()
     {
-        //Timer ajastin = new Timer();
-        //ajastin.Interval = 1.5;
-        //ajastin.Timeout += 
-        //ajastin.Start();
-        
         LuoAikalaskuri();
         
         Level.Background.Image = tausta_uusi;
@@ -95,7 +111,7 @@ public class NUO_TAISTELU : PhysicsGame
 
         Add(kasi);
         
-        PhysicsObject äijä = new PhysicsObject(400, 413);
+        äijä = new PhysicsObject(400, 413);
         
         äijä.X = äijä.X + - 400;
         äijä.Y = äijä.Y + 200;
@@ -124,6 +140,8 @@ public class NUO_TAISTELU : PhysicsGame
 
         Add(aloitusKortti);
         aloitusKortti.Angle = Angle.FromDegrees(14);
+        
+       
     }
 
     private void LuoKortti(Image image, double paikka)
